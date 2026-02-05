fc010b8c18f03a220bb2c58bd05cb673b3a56aee
jawad ahmad <jawadgujjar573@gmail.com>
2026-02-05 20:39:29 +0500

    update code

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI in .env.local");
+  throw new Error("❌ Please define MONGODB_URI in environment variables");
 }
 
 let cached = global.mongoose;
@@ -16,10 +16,11 @@ async function dbConnect() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongoose) => mongoose);
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
