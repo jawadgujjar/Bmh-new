d38f68d4fb333b0335aadce4b3ee754b63952b35
jawad ahmad <jawadgujjar573@gmail.com>
2026-04-03 20:37:06 +0500

    content missing

--- src/components/landing/faqs.js
+++ src/components/landing/faqs.js
@@ -94,7 +94,7 @@ function FAQ() {
         <h2>Still have questions?</h2>
         <p>
           Contact us directly at{" "}
-          <a href="mailto:hello@brandmarketinghub.com">hello@brandmarketinghub.com</a>
+          <a href="mailto:hello@brandmarketinghub.com">hello@brandmarketinghub.com</a>{" "}
           or call <a href="tel:+18132140535">(813) 214-0535</a>
         </p>
       </div>
