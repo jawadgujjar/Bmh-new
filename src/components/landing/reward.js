73fd84d7dc594fbfde379da2fc580756a5b03934
jawad ahmad <jawadgujjar573@gmail.com>
2026-04-23 20:51:35 +0500

    dark mode progress

--- src/components/landing/reward.js
+++ src/components/landing/reward.js
@@ -4,21 +4,25 @@ import styles from '../../styles/reward.module.css';
 
 function Reward() {
     return (
-        <div className={styles.rewardBody}>
-            <h3 className={styles.heading}>Award Winning digital <span className={styles.bolderText}>Marketing Agency</span></h3>
-            <p className={styles.textH6}>Brand's is a digital marketing and SEO company specializing in web and mobile application development with a track record of proven results.</p>
+        <section className={styles.rewardBody}>
+            <h3 className={styles.heading}>
+                Award Winning digital <span className={styles.bolderText}>Marketing Agency</span>
+            </h3>
+            <p className={styles.textH6}>
+                Brand Marketing Hub is a digital marketing and SEO company specializing in web and mobile application development with a track record of proven results.
+            </p>
             <div className={styles.imageContainer}>
                 <Image 
-                    alt='digital' 
+                    alt='Award Winning Agency Partnerships' 
                     src='/images/awards.jpg' 
                     className={styles.imageAwards}
                     width={1200}
-                    height={600}
-                    layout="responsive"
+                    height={300}
                     priority
+                    style={{ height: 'auto', width: '100%' }}
                 />
             </div>
-        </div>
+        </section>
     )
 }
 
